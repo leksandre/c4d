56b6bde18c6a7e3de0f7e327dba76ef26d597432
leksandre <44774042+leksandre@users.noreply.github.com>
2024-04-03 12:04:34 +0500

    Update domOtelScripts4.js

--- domOtelScripts4.js
+++ domOtelScripts4.js
@@ -23,7 +23,7 @@ a.setAttribute("target", "_blank");
 //only 1 time
 element.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.paddingBottom="0px";
 element.parentElement.parentElement.style.marginBottom="0px";
-
+element.style.textDecoration="underline";
 		}
 
 
@@ -41,7 +41,7 @@ while (element.firstChild) {
     	  a.href = "https://go.2gis.com/g37pa";
 a.setAttribute("target", "_blank"); 
     	  element.appendChild(a);		
-
+	  element.style.textDecoration="underline";
 		}
 
 
