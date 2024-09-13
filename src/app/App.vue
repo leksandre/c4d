6a223ba3abc3348d0261bc6254da137706edd2ba
aleksandrivanov <jirdis@ya.ru>
2024-09-13 19:38:02 +0500

    new log for "extint/send" func

--- src/app/App.vue
+++ src/app/App.vue
@@ -1224,8 +1224,110 @@ function checkCookie(cname) {
         }, 50)
       }
 
+
+
+
+      window.addEventListener("DOMContentLoaded", (event) => {
+        let chatIcons1 = parent.document.getElementById('chatIcons');
+        if (typeof chatIcons1 !== "undefined") {
+
+          var newdiv = parent.document.createElement('div');
+          newdiv.id = "chatIcons";
+          newdiv.style.background = "#e47654";
+
+          newdiv.style.width = "65px";
+          newdiv.style.zIndex = "9001";
+          newdiv.style.height = "65px";
+          newdiv.style.position = "fixed";
+          newdiv.style.right = "20px";
+          newdiv.style.bottom = "20px";
+          newdiv.style.borderRadius = "50px";
+          newdiv.style.padding = "15px";
+          newdiv.style.paddingTop = "17px";
+          newdiv.onclick = function () {
+            let elementWithIcons = parent.document.getElementById('18iconsChat');
+
+            clearInterval(timer1);
+
+            if (elementWithIcons.style.display === 'block') {
+              elementWithIcons.style.display = "none";
+            } else {
+              elementWithIcons.style.display = "block";
+            }
+          };
+
+
+          newdiv.innerHTML = '<div id="18iconsChat" style="display: none; position: absolute;bottom: 60px;right: 0px;width: 65px;height: 65px;" >'
+              + '<svg id="chat_telegram" style="cursor:pointer; " xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 240.1 240.1"  height="65px" width="65px" >'
+              + '<linearGradient id="Oval_1_" gradientUnits="userSpaceOnUse" x1="-838.041" y1="660.581" x2="-838.041" y2="660.3427" gradientTransform="matrix(1000 0 0 -1000 838161 660581)">'
+              + ' <stop offset="0" style="stop-color:#2AABEE"/>'
+              + ' <stop offset="1" style="stop-color:#229ED9"/>'
+              + '</linearGradient>'
+              + '<circle fill-rule="evenodd" clip-rule="evenodd" fill="url(#Oval_1_)" cx="120.1" cy="120.1" r="120.1"/>'
+              + '<path fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M54.3,118.8c35-15.2,58.3-25.3,70-30.2 c33.3-13.9,40.3-16.3,44.8-16.4c1,0,3.2,0.2,4.7,1.4c1.2,1,1.5,2.3,1.7,3.3s0.4,3.1,0.2,4.7c-1.8,19-9.6,65.1-13.6,86.3' + 'c-1.7,9-5,12-8.2,12.3c-7,0.6-12.3-4.6-19-9c-10.6-6.9-16.5-11.2-26.8-18c-11.9-7.8-4.2-12.1,2.6-19.1c1.8-1.8,32.5-29.8,33.1-32.3 c0.1-0.3,0.1-1.5-0.6-2.1c-0.7-0.6-1.7-0.4-2.5-0.2c-1.1,0.2-17.9,11.4-50.6,33.5c-4.8,3.3-9.1,4.9-13,4.8' + 'c-4.3-0.1-12.5-2.4-18.7-4.4c-7.5-2.4-13.5-3.7-13-7.9C45.7,123.3,48.7,121.1,54.3,118.8z"/>'
+              + '</svg>'
+              + '</div>'
+              + '<svg style="cursor:pointer; " fill="#fff" height="35px" width="35px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" '
+              + 'viewBox="0 0 60 60" xml:space="preserve">'
+              + '<g>'
+              + '<path d="M44.348,12.793H2.652C1.189,12.793,0,13.982,0,15.445v43.762l9.414-9.414h34.934c1.463,0,2.652-1.19,2.652-2.653V15.445'
+              + 'C47,13.982,45.811,12.793,44.348,12.793z M10,35.777c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S12.206,35.777,10,35.777z'
+              + ' M23,35.777c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S25.206,35.777,23,35.777z M36,35.777c-2.206,0-4-1.794-4-4s1.794-4,4-4'
+              + 's4,1.794,4,4S38.206,35.777,36,35.777z"/>'
+              + '<path d="M57.348,0.793H12.652C11.189,0.793,10,1.982,10,3.445v7.348h34.348c2.565,0,4.652,2.087,4.652,4.652v25.332h11V3.445'
+              + 'C60,1.982,58.811,0.793,57.348,0.793z"/>'
+              + '</g>'
+              + '</svg>'
+              + '';
+          parent.document.body.appendChild(newdiv);
+
+          let elementWithIcontelegramm = parent.document.getElementById('chat_telegram');
+          elementWithIcontelegramm.onclick = function () {
+            let url = "https://t.me/DomOtel_Tmn_bot";
+            //window.location.replace(url);
+            //window.location.href = url;
+            window.open(url, '_blank').focus();
+          }
+
+
+          const timer1 = setInterval(() => {
+            this.swalScale(newdiv);
+          }, 3000);
+          setTimeout(function () {
+            clearInterval(timer1);
+          }, 30000);
+
+        }
+      });
+
+
+
+
     },
     methods: {
+
+
+      async swalScale(el) {
+
+        el
+            .animate({ opacity: [1, 0], transform: ["scale(1)", "scale(0)"], }, { duration: 1000, iterations: 1, easing: "ease-in" })
+            .onfinish = (e) => {
+
+          e.target.effect.target.style.opacity = 0;
+          e.target.effect.target.style.transform = 'scale(0)';
+
+          el
+              .animate({ opacity: [0, 1], transform: ["scale(0)", "scale(1)"], }, { duration: 1000, iterations: 1, easing: "ease-in" })
+              .onfinish = (e) => {
+
+            e.target.effect.target.style.opacity = 1;
+            e.target.effect.target.style.transform = 'scale(1)';
+
+          };
+
+        };
+
+      },
 
       goToDomRf(){
         let url = 'https://xn--h1alcedd.xn--d1aqf.xn--p1ai/calculators/kalkulyator-ipoteki/'
@@ -1617,7 +1719,7 @@ function checkCookie(cname) {
         //extract utm marks
        let forValEvents = this.extractUtmMarks(sqr)
 
-        console.log('JSON.stringify',JSON.stringify(forValEvents))
+        //console.log('JSON.stringify',JSON.stringify(forValEvents))
 
         document.cookie = "lastSqrApartament="+sqr;
         let details = {
@@ -1647,7 +1749,7 @@ function checkCookie(cname) {
         })   .then((responseNew) => responseNew.json())
             .then((resJson) => {
 
-              console.log('c event json - ',resJson)
+              //console.log('c event json - ',resJson)
 
             });
 
--- src/app/components/ApartmentDetails.vue
+++ src/app/components/ApartmentDetails.vue
@@ -222,12 +222,7 @@
 
 
         <button type="button" v-on:click="setModalDialog" class="buttonCopy2" data-callback="true" data-href="zabronirovat-kvartiru" aria-label="Задать вопрос" data-init="true"  style="  position: relative; left: 21px;">
-          <span style="position: absolute;
-    left: 26px;
-    font-size: 20px;
-    font-weight: 900;
-    font-style: normal;
-    font-stretch: ultra-condensed;">?</span>
+          <span data-v-c4a012b6="" style="position: absolute; left: 15px; font-size: 20px; font-weight: 900; font-style: normal; font-stretch: ultra-condensed;"><svg data-v-c4a012b6="" fill="#fff" height="25px" width="25px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 60 60" xml:space="preserve" style="cursor: pointer;"><g data-v-c4a012b6=""><path data-v-c4a012b6="" d="M44.348,12.793H2.652C1.189,12.793,0,13.982,0,15.445v43.762l9.414-9.414h34.934c1.463,0,2.652-1.19,2.652-2.653V15.445C47,13.982,45.811,12.793,44.348,12.793z M10,35.777c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S12.206,35.777,10,35.777z M23,35.777c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S25.206,35.777,23,35.777z M36,35.777c-2.206,0-4-1.794-4-4s1.794-4,4-4s4,1.794,4,4S38.206,35.777,36,35.777z"></path> <path data-v-c4a012b6="" d="M57.348,0.793H12.652C11.189,0.793,10,1.982,10,3.445v7.348h34.348c2.565,0,4.652,2.087,4.652,4.652v25.332h11V3.445C60,1.982,58.811,0.793,57.348,0.793z"></path></g></svg></span>
           <span class="buttonCopy2span" >Задать вопрос</span>
         </button>
 
@@ -384,6 +379,9 @@
                 <div class="dialogAsc-wrapper">
                   <div class="dialogAsc-container">
                     <div class="dialogAsc-header">
+
+                      <div data-v-c4a012b6="" id="18iconsChat" onclick="window.open('https://t.me/DomOtel_Tmn_bot', '_blank').focus();" style="float: right; right: 0px; width: 45px; height: 45px;"><svg data-v-c4a012b6="" id="chat_telegram" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 240.1 240.1" height="45px" width="45px" style="cursor: pointer;"><linearGradient data-v-c4a012b6="" id="Oval_1_" gradientUnits="userSpaceOnUse" x1="-838.041" y1="660.581" x2="-838.041" y2="660.3427" gradientTransform="matrix(1000 0 0 -1000 838161 660581)"><stop data-v-c4a012b6="" offset="0" style="stop-color: rgb(42, 171, 238);"></stop> <stop data-v-c4a012b6="" offset="1" style="stop-color: rgb(34, 158, 217);"></stop></linearGradient> <circle data-v-c4a012b6="" fill-rule="evenodd" clip-rule="evenodd" fill="url(#Oval_1_)" cx="120.1" cy="120.1" r="120.1"></circle> <path data-v-c4a012b6="" fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M54.3,118.8c35-15.2,58.3-25.3,70-30.2 c33.3-13.9,40.3-16.3,44.8-16.4c1,0,3.2,0.2,4.7,1.4c1.2,1,1.5,2.3,1.7,3.3s0.4,3.1,0.2,4.7c-1.8,19-9.6,65.1-13.6,86.3c-1.7,9-5,12-8.2,12.3c-7,0.6-12.3-4.6-19-9c-10.6-6.9-16.5-11.2-26.8-18c-11.9-7.8-4.2-12.1,2.6-19.1c1.8-1.8,32.5-29.8,33.1-32.3 c0.1-0.3,0.1-1.5-0.6-2.1c-0.7-0.6-1.7-0.4-2.5-0.2c-1.1,0.2-17.9,11.4-50.6,33.5c-4.8,3.3-9.1,4.9-13,4.8c-4.3-0.1-12.5-2.4-18.7-4.4c-7.5-2.4-13.5-3.7-13-7.9C45.7,123.3,48.7,121.1,54.3,118.8z"></path></svg></div>
+
 <!--                      <h6 name="header">Укажите пожалуйста ваши данные и номер телефона </h6>-->
 <!--                      <h6 name="header">Укажите пожалуйста ваши данные </h6>-->
                       <h6 name="header">Заполните</h6>
@@ -850,6 +848,48 @@ function checkCookie(cname) {
 
       },
 
+
+      async createEventTenant(phone,name,data) {
+
+        let objId = 18
+        let tk = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkb21vdGVsX3N0YXRzIiwibmJmIjoxNzE4MzYxODY2LCJleHAiOjEwMTcxMDc4MTA2NiwidWlkIjoiYmQyYWZjYTEtYmZmNi00NWM3LTlhNTEtYTQ2ZWNlMjBjNGZmIiwic3ViIjoid2lkZ2V0cyIsIm9ianV1aWQiOiJkODQ2NmM0Yi0wYTk3LTQzM2QtOTk4Yy02NmFkZjIwNzI1NGYiLCJ1c2VydXVpZCI6IiJ9.wGgtRB-6kIAL4T1sSyYDN94YUIYyIsMs7iAIq1X9Jac'
+        let forValEvents = {phone,name,data}
+
+
+        let details = {
+          'ApplicationId':2,
+          'ObjectId':objId,
+          'StatusId':3,
+          'ActionName':'сообщение с сайта',
+          'Value':JSON.stringify(forValEvents),
+        }
+        var formBody = [];
+        for (var property in details) {
+          var encodedKey = encodeURIComponent(property);
+          var encodedValue = encodeURIComponent(details[property]);
+          formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
+
+        fetch("https://domotel_stats-admin.mobsted.ru/api/v8/events", {
+          method: "POST",
+          body: formBody,
+          headers: {
+            // 'Content-Type': 'application/json;charset=utf-8',
+            // 'Content-Type': 'text/plain;charset=UTF-8',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            "authorization": "Bearer "+tk
+          }
+        })   .then((responseNew) => responseNew.json())
+            .then((resJson) => {
+
+              //console.log('c event json - ',resJson)
+
+            });
+
+      },
+
+
       sendMessage1() {
         // console.log(this.message1)
         // console.log(this.phone1)
@@ -905,7 +945,6 @@ function checkCookie(cname) {
 
         // console.log('dataforsend',dataforsend)
 
-
         let res1 = fetch("https://do72.4dev.app/do72api/hs/extint/send", {
           method: "POST",
           body: JSON.stringify({
@@ -928,8 +967,8 @@ function checkCookie(cname) {
           }
         });
 
-        console.log(res1) // в случе успеха придет - success
-
+        // console.log(res1) // в случе успеха придет - success
+        this.createEventTenant(this.phone1,this.name1,JSON.stringify(dataforsend))
         this.setModalDialog()
 
         window.createNotification({
